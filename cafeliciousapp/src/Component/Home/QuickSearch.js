bca31377af2f97c8b1e5b796cee4f120f5b0be0b
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-12-08 20:23:28 +0800

    Header Footer

--- cafeliciousapp/src/Component/Home/QuickSearch.js
+++ cafeliciousapp/src/Component/Home/QuickSearch.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-// import './QuickSearch.css';
+import './QuickSearch.css';
 import QuickDisplay  from './QuickDisplay';
 
 const baseUrl = "http://localhost:9120";
--- cafeliciousapp/src/Component/Routing.js
+++ cafeliciousapp/src/Component/Routing.js
@@ -2,18 +2,23 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Main from './Main';
 import Home from './Home/Home';
+import Header from './Header';
+import Footer from './Footer';
 
 
 const Routing = () => {
     return(
         <>
             <BrowserRouter>
+            <Header/>
 
                 <Routes>
+
                     <Route path="/" element={<Main/>}>
                         <Route index element={<Home/>}/>
                     </Route>
                 </Routes>
+            <Footer/>
 
             </BrowserRouter>
         </>
