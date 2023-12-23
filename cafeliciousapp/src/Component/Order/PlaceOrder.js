b4ec7b3d9616d5cbb55d514757de9ee10f1760d7
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-12-23 17:24:15 +0800

    data.json

--- cafeliciousapp/src/Component/Order/PlaceOrder.js
+++ cafeliciousapp/src/Component/Order/PlaceOrder.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import { useParams,useNavigate} from 'react-router-dom';
-import Header from '../Header';
+import Header2 from '../Header2';
 
 const baseUrl = "http://localhost:9120";
 
@@ -46,7 +46,7 @@ const PlaceOrder = () => {
 
     return(
         <>
-            {/* <Header/> */}
+            <Header2/>
             <div className="container">
                 <hr/>
                 <div className="panel panel-primary">
