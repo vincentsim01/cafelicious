af600ddd0c0b62fae3771ebaa779a39b1cbb3d5c
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-01-19 21:22:58 +0800

    promo

--- cafeliciousapp/src/Component/promotion/promotion.js
+++ cafeliciousapp/src/Component/promotion/promotion.js
@@ -14,6 +14,9 @@ const QuickPromotion = () => {
 
             <span className="PromotionHeader">Promotion This Month</span><br></br>
             <span className="PromotionSubHeader">Don't miss our special deal this month! buy from any store and get free flower banquet</span>
+            <img src="https://i.ibb.co/TRJxTcT/Free-Flower-For-Any-Purchase.png"></img>
+
+            {/* <a href="https://ibb.co/K7BpG6G"><img src="https://i.ibb.co/TRJxTcT/Free-Flower-For-Any-Purchase.png" alt="Free-Flower-For-Any-Purchase" border="0"></a> */}
 
         </div>
         </>
